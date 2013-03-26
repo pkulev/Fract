78e07291995beb4f1c451eee9d7e15c94a126072
MoSt <kulyov.pavel@gmail.com>
2013-03-26 13:20:16 +0000

    test

--- all.py
+++ all.py
@@ -1,5 +1,16 @@
+import sys
+import pygame
+
+#view
+pygame.init()
+width = 640
+height = 480
+window = pygame.display.set_mode((width, height))
+
+#virtual machine implementation
+
 from math import sin, cos, pi
-#
+
 def GenProgram(current, rules, depth):
     if depth == 0:
         return current
@@ -72,3 +83,25 @@ def draw(program, cur_x, cur_y, alf, d_alf, step):
             if event.type == pygame.QUIT: 
                 sys.exit(0) 
             else: pass
+
+
+#executable
+
+inp = "auto"  # manual or auto
+if inp == "auto":
+    rules = dict(P = "F+F+F+F")
+    program = "P"
+else:
+    rnumb = raw_input("[how much]")
+    rules = dict()
+#TODO: user input        
+        
+step = 30
+deep = 5
+alf = 0.0
+d_alf = 90.0
+
+
+program = GenProgram(program, rules, deep)
+
+draw(program, width / 2, height / 2, alf,  d_alf, step)
