267c353ba024f28665b5f0f88ae34aec6170c829
pankshok <kulyov.pavel@gmail.com>
2013-05-04 15:09:42 +0400

    .

--- hw.py
+++ hw.py
@@ -19,54 +19,80 @@ TEAL      =  (  0, 128, 128)
 WHITE     =  (255, 255, 255)
 YELLOW    =  (255, 255,   0)
 
+class game(object):
+
+    def __init__(self):
+        self.FPS = 60
+        self.fpsClock = pygame.time.Clock()
+        
+        pygame.init()
+        self.display = pygame.display.set_mode((400, 300), 0, 32)
+        pygame.display.set_caption('Animation')
+        self.mycat = self.catImg = pygame.image.load('res/cat.png')
+        
+        self.catx = 10
+        self.caty = 10
+        self.direction = 'right'
+        self.fontObj = pygame.font.Font('freesansbold.ttf', 16)
+        self.mytext = self.fontObj.render("L", True, RED, WHITE)
+        self.mytextRect = self.mytext.get_rect()
+        self.mytextRect.center = (self.mycat.get_width() / 2, self.mycat.get_height() / 2)
+        self.textSurfaceObj = self.fontObj.render(self.direction, True, BLACK, WHITE)
+        self.textRectObj = self.textSurfaceObj.get_rect()
+        self.textRectObj.center = (200, 150)
+
+    def init(self):
+        pass
+
+    def quit(self):
+        pass
+    def setText(self,string):
+        self.mytext = self.fontObj.render(str(id(self.catImg) == id(self.mycat)),True,BLACK,WHITE)#string, True, textSurfaceObj  BLACK, WHITE)
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            else:
+                print(event)
 
 
+    def updateState(self):
+        if self.direction == 'right':
+            self.catx += 5
+            if self.catx == 280:
+                self.direction = 'down'
+        elif self.direction == 'down':
+            self.caty += 5
+            if self.caty == 220:
+                self.direction = 'left'
+        elif self.direction == 'left':
+            self.catx -= 5
+            if self.catx == 10:
+                self.direction = 'up'
+        elif self.direction == 'up':
+            self.caty -= 5
+            if self.caty == 10:
+                self.direction = 'right'
+        
+
+    def drawScreen(self):
+        self.display.fill(WHITE)
+        self.setText(self.direction)
+        self.catImg.blit(self.mytext, self.mytextRect)
+        self.display.blit(self.mycat, ((self.display.get_width() - self.catImg.get_width()) / 2, (self.display.get_height() - self.catImg.get_height()) / 2))
+        #self.display.blit(self.textSurfaceObj, self.textRectObj)
+        
+        self.display.blit(self.catImg, (self.catx, self.caty))
+    
+        pygame.display.update()
+        self.fpsClock.tick(self.FPS)
 
 
+catgame = game()
 
-
-
-
-
-
-
-
-def handleEvents():
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        else:
-            print(event)
-
-def updateState():
-    pass
-
-def drawScreen():
-    display.fill(WHITE)
-    pygame.draw.polygon(display, GREEN, ((143, 0), (291, 106), (236, 277),
-                                         (56, 277), (0, 166)))
-    pygame.draw.line(display, BLUE, (60, 60), (120, 60), 4)
-    pygame.draw.line(display, BLUE, (120, 60), (60, 120))
-    pygame.draw.line(display, BLUE, (60, 120), (120, 120), 4)
-    pygame.draw.circle(display, BLUE, (300, 50), 20, 0)
-    pygame.draw.ellipse(display, RED, (300, 250, 40, 80), 1)
-    pygame.draw.rect(display, RED, (200, 150, 100, 50))
-
-    PixObj = pygame.PixelArray(display)
-    PixObj[480][380] = BLACK
-    PixObj[482][382] = BLACK
-    PixObj[484][384] = BLACK
-    PixObj[486][386] = BLACK
-    PixObj[488][388] = BLACK
-    del PixObj
-    pygame.display.update()
-
-pygame.init()
-display = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Drawing')
 while True:
-    handleEvents()
-    updateState()
-    drawScreen()
-
+    catgame.handleEvents()
+    catgame.updateState()
+    catgame.drawScreen()
