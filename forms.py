7246d599d879f6557f70cc27b7cd9c9a480cff39
Pavel Kulyov <kulyov.pavel@gmail.com>
2017-06-20 20:19:33 +0300

    Replace bunch or rules with one
    Signed-off-by: Pavel Kulyov <kulyov.pavel@gmail.com>

--- forms.py
+++ forms.py
@@ -6,10 +6,7 @@ from wtforms.validators import DataRequired
 class FractalArgumentsForm(FlaskForm):
     depth = IntegerField("depth")
     program = StringField("program")
-    rule1 = StringField("rule 1")
-    rule2 = StringField("rule 2")
-    rule3 = StringField("rule 3")
-    # TODO: add some else
+    rules = StringField("rules")
     start_angle = FloatField("start angle")
     delta_angle = FloatField("delta angle")
     step = IntegerField("step")
